4540d2c014472b93582ef51bf5f8159d97672ce0
Felipe <profissionalf.andre@gmail.com>
2020-09-12 14:14:28 -0300

    asas

--- src/components/Burger/index.jsx
+++ src/components/Burger/index.jsx
@@ -1,19 +1,19 @@
 import './styled.css'
 import React from 'react'
-import { actViewMenu } from '../../store/landing/action'
-import { useDispatch } from 'react-redux'
+import { actViewMenu } from '../../store/landing/action';
+import { useDispatch } from 'react-redux';
 const Burger = () => {
   const dispatch = useDispatch()
   const handleOpenMenu = () => {
-    dispatch(actViewMenu())
-  }
-  return (
-    <>
-      <input id="menu-hamburguer" type="checkbox" />
-      <label htmlFor="menu-hamburguer">
-        <div className="menu">
-          <span onClick={handleOpenMenu} className="hamburguer"></span>
-        </div>
+                  dispatch(actViewMenu())
+                }
+                return (
+                  <>
+                    <input id="menu-hamburguer" type="checkbox" />
+                    <label htmlFor="menu-hamburguer">
+                      <div className="menu">
+                        <span onClick={handleOpenMenu} className="hamburguer"></span>
+                      </div>
       </label>
     </>
   )
